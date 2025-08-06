0043f03a85c370a46930e7b8f3996d31d35eb713
gitroll2023 <class2023.05@gmail.com>
2025-08-06 17:06:00 +0900

    s

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import { Header } from '@/components/layout/Header'
-import { Footer } from '@/components/layout/Footer'
+import { Footer } from '@/components/layout/footer'
 
 export default function MainLayout({
   children,
